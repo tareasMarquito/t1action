f0021752dd92fe5d3cff729b0b8948b53aca5213
Bonihub <lordboni@hotmail.com>
2025-08-04 11:22:18 -0600

    feat: se actualiza el yml

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const core = require('@actions/core');
 try {
     const name = core.getInput('NAME');
+    const age = core.getInput('AGE');
+    const lastname = core.getInput('APELLIDO');
     console.log(`Hello ${name}!`);
+    console.log(`You are ${age} years old.`);
+    console.log(`Your last name is ${lastname}.`);
 } catch (error) {
     core.setFailed(`Action failed with error: ${error.message}`);
 }
